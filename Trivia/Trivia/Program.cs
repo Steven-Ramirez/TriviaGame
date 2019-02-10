19631fa84bf5870e118f8c7e348d343a87afead3
Steven-Ramirez <steve.ramirez@loop.colum.edu>
2019-02-10 14:01:53 -0600

    Made changes to List of questions

--- Trivia/Trivia/Program.cs
+++ Trivia/Trivia/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Trivia
 {
@@ -10,7 +11,7 @@ namespace Trivia
     {
         static void Main(string[] args)
         {
-
+            Console.ReadLine();
         }
     }
 }
--- Trivia/Trivia/Questions.cs
+++ Trivia/Trivia/Questions.cs
@@ -17,30 +17,36 @@ namespace Trivia
     {
         Questions()
         {
+            Welcome();
+        }
 
+        public void Welcome()
+        {
+            Console.WriteLine("Hello and welcome to Stevens Trivia Game!");
+            Console.WriteLine("This quiz will test you on three different subjects, History, Geography, and Music!");
+            Console.Write("Which subject would you like to be tested in? (H)istory, (G)eography, or (M)usic: ");
+            string answer = Console.ReadLine();
         }
 
         public void Question()
         {
-            string[] questions = {"In what year did the walls of Constaninople fall to Ottoman Empire?" +
-                              "What year was the Constitution of The United States written?" +
-                              "Which Soviet satellite was the first to be launched into space in 1957?" +
-                              "What is the earliest surviving system of laws?" +
-                              "The Treaty of Versailles followed which war?" +
-                              "What is Earth's largest continent?" +
-                              "In what country can you visit Machu Picchu?" +
-                              "What is the largest country on the Arabian Peninsula?" +
-                              "What is the tallest mountain in the world?" +
-                              "What is the fastest flowing river in the world?" +
-                              "Which country is NOT part of the Scaninavian Peninsula?" +
-                              "What pop singer is known as The Material Girl?" +
-                              "What band did Prince form in 1981?" +
-                              "What was the band known as Linkin Park originally called?" +
-                              "What singer holds the world record for most words in a hit single?" +
-                              "What singer is known as the Empress of the Blues?"}; // question from http://www.usefultrivia.com/history_trivia/index_iv.html
+            List<string> history = new List<string>();
+            string[] fileHistoryQuestions = File.ReadAllLines("History.txt"); // question from http://www.usefultrivia.com/history_trivia/index_iv.html
+            history.AddRange(fileHistoryQuestions);
 
-            string[] answers = {"1453", "1787", "Sputnik", "Code of Hammurabi", "World War 1", "Asia", "Peru", "Saudia Arabia", "Mount Everest", "Amazon" +
-                            "Denmark", "Madonna", "The Time", "Xeno" , "Eminem", "Bessie Smith"}; // answers from http://www.usefultrivia.com/history_trivia/index_iv.html
+            List<string> geo = new List<string>();
+            string[] fileGeoQuestions = File.ReadAllLines("Geo.txt"); // answers from http://www.usefultrivia.com/history_trivia/index_iv.html*/
+            geo.AddRange(fileGeoQuestions);
+
+            List<string> music = new List<string>();
+            string[] fileMusicQuestions = File.ReadAllLines("Music.txt"); // answers from http://www.usefultrivia.com/history_trivia/index_iv.html*/
+            music.AddRange(fileMusicQuestions);
+
+        }
+
+        public void AddQuestions()
+        {
+
         }
         
 
